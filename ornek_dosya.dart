5cb45784e85cfea0663ec6a9998fc8fc8ea06344
denizdogruel00 <denizyazilm@gmail.com>
2026-04-17 18:42:53 +0300

    final

--- ornek_dosya.dart
+++ ornek_dosya.dart
@@ -1,7 +1,15 @@
 void main(List<String> args) {
+  //1
   var birincikenari=3;
   var ikincikenari=2;
   var ucuncukenar=7;
   var cevre=ucuncukenar+birincikenari+ikincikenari;
   print("Çevre:$cevre");
+  //2
+  var ad="deniz";
+  var soyad="doğruel";
+  var dogumyili=2001;
+  var guncelyil=2026;
+  var yas=guncelyil-dogumyili;
+  print("isim $ad ve soyad $soyad dogumyilim $dogumyili guncelyil $guncelyil ve yasim $yas");
 }
